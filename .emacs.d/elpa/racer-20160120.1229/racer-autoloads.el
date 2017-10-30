e5a3428a896a3042cea5342d55ec4ea96962308f
David Young-chan Kay <davidykay@gmail.com>
2017-10-29 21:39:55 -0700

    Deleted old config and moving to new one. Not sure this is a good idea.

--- .emacs.d/elpa/racer-20160120.1229/racer-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; racer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (racer-mode racer-find-definition) "racer" "racer.el"
-;;;;;;  (22230 22369 634429 331000))
-;;; Generated autoloads from racer.el
-
-(autoload 'racer-find-definition "racer" "\
-Run the racer find-definition command and process the results.
-
-\(fn)" t nil)
-
-(autoload 'racer-mode "racer" "\
-Minor mode for racer.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("racer-pkg.el") (22230 22369 653411 909000))
-
-;;;***
-
-(provide 'racer-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; racer-autoloads.el ends here
