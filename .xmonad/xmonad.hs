c16f19d9cc9dccf111a214f09f7cd503920b2e69
David Young-chan Kay <davidykay@gmail.com>
2018-10-25 10:57:53 -0700

    Now using Spiral and Mosaic layouts from Xmonad contrib

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,22 @@
 import XMonad
+import XMonad.Layout.Spiral
+import XMonad.Layout.Mosaic
+
+myLayout = tiled ||| Mirror tiled ||| spiral (6/7) ||| mosaic 2 [3,2] ||| Full
+  where
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
+
+    -- The default number of windows in the master pane
+    nmaster = 1
+
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
+
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
 
 main = xmonad def
-    { terminal    = "gnome-terminal" }
+    { terminal = "gnome-terminal",
+      layoutHook = myLayout
+    }
