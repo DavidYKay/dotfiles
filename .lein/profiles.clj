2fd30cec02b94b6a0c2b71d6ec41356f2ae1afc8
David Young-chan Kay <davidykay@gmail.com>
2018-11-09 20:35:22 -0800

    Added leiningen profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,8 @@
+ {:user {:plugins [[cider/cider-nrepl "0.18.0"]
+                   [lein-localrepo "0.5.2"]
+                   [lein-midje "3.2.1"]
+                   [com.jakemccrary/lein-test-refresh "0.23.0"]
+                   [metosin/bat-test "0.4.0"]
+                   [venantius/ultra "0.5.2"]
+                   ;;[lein-ns-dep-graph "0.2.0-SNAPSHOT"]
+                   ]}}
